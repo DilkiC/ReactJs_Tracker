cd401db5a34fb5c84b540905efdd1abb49fec68a
DilkiC <dilkicooray97@gmail.com>
2021-11-19 15:32:51 +0530

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Income Tracker</h1>
+
+      <div className="total-income">Ru.400
+
+      </div>
+      
+    </header>
+  )
+}
+
+export default Header;
+
